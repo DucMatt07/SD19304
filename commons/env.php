883275dcf8fc2fce11d2934bb6dfe6a45ebfc070
DucMatt07 <142436621+DucMatt07@users.noreply.github.com>
2025-03-27 08:42:24 +0700

    sprint 4

---
+++ commons/env.php
@@ -0,0 +1,15 @@
+<?php
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'   , 'http://localhost/shop_giay/adadisXshop/');
+define('BASE_URL_ADMIN'       , 'http://localhost/shop_giay/adadisXshop/admin/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_NAME'    , 'shop_giay');  // Tên database
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
